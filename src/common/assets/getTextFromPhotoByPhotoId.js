ba4e4468239d77e5c995913bce5d5167907cfc98
Igor <ulanov.igor0400@gmail.com>
2023-10-01 14:16:15 +0300

    commit

--- src/common/assets/getTextFromPhotoByPhotoId.js
+++ src/common/assets/getTextFromPhotoByPhotoId.js
@@ -1,27 +1,16 @@
 import Tesseract from 'tesseract.js';
 import { bot } from '../../../settings.js';
-import { isValidText } from './isValidText.js';
 
 export const getTextFromPhotoByPhotoId = async (photoId) => {
    try {
       const downloadUrl = await bot.telegram.getFileLink(photoId);
       const photoUrl = downloadUrl.href;
 
-      // сделать распознование рукописного текста
-
       const {
          data: { text },
-      } = await Tesseract.recognize(photoUrl, 'rus');
+      } = await Tesseract.recognize(photoUrl, 'rus+eng');
 
-      const isValid = await isValidText(text);
-
-      if (text && isValid) return text;
-
-      const {
-         data: { text: newText },
-      } = await Tesseract.recognize(photoUrl, 'eng');
-
-      if (newText) return newText;
+      return text;
    } catch (e) {
       console.log(e);
    }
--- src/recognite-solution/photo.js
+++ src/recognite-solution/photo.js
@@ -1,12 +1,19 @@
 import { generateSolutionByText } from './assets/index.js';
-import { getTextFromPhotoByPhotoId } from '../common/index.js';
+import { getTextFromPhotoByPhotoId, sendLoading } from '../common/index.js';
+import { bot } from '../../settings.js';
 
 export const recogniteSolutionByPhoto = async (ctx) => {
    const message = ctx.update.message;
    const photo = message.photo[message.photo.length - 1];
 
+   const loading = await sendLoading(ctx, undefined, '🖼 Распознаю фото');
+
    try {
       const text = await getTextFromPhotoByPhotoId(photo.file_id);
+
+      const { chatId, messageId } = loading;
+      loading.stop();
+      await bot.telegram.deleteMessage(chatId, messageId);
 
       if (text) {
          await generateSolutionByText(ctx, text);
